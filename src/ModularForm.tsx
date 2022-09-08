c31d20e57624396ef19c858a8533308da7217b43
Jérémie Dubuis <jeremie.dubuis@gmail.com>
2022-09-08 10:51:26 +0200

    :sparkles: add form ref

--- src/ModularForm.tsx
+++ src/ModularForm.tsx
@@ -1,31 +1,40 @@
-import React from 'react';
-import { FormStore } from './FormStore';
-import type { ModularFormProps } from './types';
+import React, { ForwardedRef } from "react";
+import { FormStore } from "./FormStore";
+import type { ModularFormProps } from "./types";
 
-export const ModularForm: React.FC<ModularFormProps> = ({
-    children,
-    id,
-    onSubmit,
-    onSubmitError,
-    handleSameNameFieldValues,
-    parseAccessors,
-    ...intrinsic
-}) => (
+export const ModularForm: React.FC<ModularFormProps> = React.forwardRef(
+  (
+    {
+      children,
+      id,
+      onSubmit,
+      onSubmitError,
+      handleSameNameFieldValues,
+      parseAccessors,
+      ...intrinsic
+    },
+    ref: ForwardedRef<FormStore>
+  ) => (
     <form
-        id={id}
-        {...intrinsic}
-        onSubmit={(e) => {
-            const form = FormStore.getForm(id);
-            form.set(handleSameNameFieldValues, parseAccessors);
-            const errors = form.getErrors(false);
-            if (!errors.length) {
-                onSubmit?.(e, form.getValues());
-            } else {
-                e.preventDefault();
-                onSubmitError?.(e, errors);
-            }
-        }}
+      id={id}
+      {...intrinsic}
+      onSubmit={(e) => {
+        const form = FormStore.getForm(id);
+        if (ref) {
+          if (typeof ref === "function") ref(form);
+          else ref.current = form;
+        }
+        form.set(handleSameNameFieldValues, parseAccessors);
+        const errors = form.getErrors(false);
+        if (!errors.length) {
+          onSubmit?.(e, form.getValues());
+        } else {
+          e.preventDefault();
+          onSubmitError?.(e, errors);
+        }
+      }}
     >
-        {children}
+      {children}
     </form>
+  )
 );
