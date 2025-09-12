b58f0bf304bcf0fd26c748a2965e2357b5695d94
Jérémie Dubuis <jeremie.dubuis@gmail.com>
2025-09-12 14:02:41 +0200

    :wrench: make jsx-runtime external

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       fileName: (format) => `react-modular-forms.${format}.js`
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React'
